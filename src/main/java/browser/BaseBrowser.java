d4f5bba46a344767f86163e2bfe2992813c6d97a
Timur Nigmatzyanov <timur.nigmatzyanov@skillbox.ru>
2026-03-14 17:34:08 +0300

    Task3 done

--- src/main/java/browser/BaseBrowser.java
+++ src/main/java/browser/BaseBrowser.java
@@ -1,5 +1,6 @@
 package browser;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 
 public class BaseBrowser {
@@ -12,26 +13,32 @@ public class BaseBrowser {
         driver.manage().window().maximize();
     }
 
+    @Step("Получить экземпляр WebDriver")
     public WebDriver getDriver() {
         return driver;
     }
 
+    @Step("Закрыть браузер")
     public void quit() {
         driver.quit();
     }
 
+    @Step("Перейти на главную страницу")
     public void navigateToMain() {
         driver.get(baseURL);
     }
 
+    @Step("Перейти по пути: {path}")
     public void navigateTo(String path) {
         driver.get(baseURL + path);
     }
 
+    @Step("Получить текущий URL")
     public String getCurrentUrl() {
         return driver.getCurrentUrl();
     }
 
+    @Step("Получить базовый URL")
     public String getBaseUrl() {
         return baseURL;
     }
